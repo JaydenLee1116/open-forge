ba7387de6fb6af414282fcf619974b2b5c259479
JaydenLee1116 <vv55adss@gmail.com>
2024-04-28 20:55:08 +0900

    react: 마이크로 상태관리 챕터 4 테스트

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+type Store<T> = {
+  getState: () => T;
+  setState: (action: T | ((prev: T) => T)) => void;
+  subscribe: (callback: () => void) => () => void;
+};
 
+const createStore = <T extends unknown>(initialState: T): Store<T> => {
+  let state = initialState;
+  const callbacks = new Set<() => void>();
+  const getState = () => state;
+  const setState = (nextState: T | ((prev: T) => T)) => {
+    console.log('store의 setState 함수 입니다');
+    
+    state =
+      typeof nextState === "function"
+        ? (nextState as (prev: T) => T)(state)
+        : nextState;
+    callbacks.forEach((callback) => callback());
+  };
+  const subscribe = (callback: () => void) => {
+    callbacks.add(callback);
+    return () => {
+      callbacks.delete(callback);
+    };
+  };
+  return { getState, setState, subscribe };
+};
+
+const store = createStore({ count: 0 });
+
+export const useStore = <T extends unknown>(store: Store<T>) => {
+  const [state, setState] = useState(store.getState());
+  useEffect(() => {
+    const unsubscribe = store.subscribe(() => {
+      setState(store.getState());
+    console.log('subscribe 내부 입니다');
+    });
+    console.log('subscribe 외부 입니다');
+    
+    // setState(store.getState());
+    return unsubscribe;
+  }, [store]);
+  return [state, store.setState] as const;
+};
+
+const Component1 = () => {
+  const [state, setState] = useStore(store);
+  const inc = () => {
+    setState((prev) => ({
+      ...prev,
+      count: prev.count + 1,
+    }));
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      {state.count} <button onClick={inc}>+1</button>
+    </div>
+  );
+};
 
-export default App
+const Component2 = () => {
+  const [state, setState] = useStore(store);
+  const inc2 = () => {
+    setState((prev) => ({
+      ...prev,
+      count: prev.count + 2,
+    }));
+  };
+  return (
+    <div>
+      {state.count} <button onClick={inc2}>+2</button>
+    </div>
+  );
+};
+
+const App = () => (
+  <>
+    <Component1 />
+    <Component2 />
+  </>
+);
+
+export default App;
